977aa9d5d4df65c1c07f47e8a1255f067ffd1224
rhkearns <ryankearns217@gmail.com>
2021-10-11 17:17:26 -0400

    create pseudocode - first pass

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/*-------------------- Constants------------------------*/
+
+// sound for clicks
+// sound for passing/failing quiz
+// objects for each quiz
+// object for each question with answer choices
+
+/*---------------------- Variables --------------------*/
+
+// Create variables for number of questions, number answered, number answered correctly
+
+
+/*----------------- Cached Element References ---------*/
+
+// Cached elements: question message; muliple choices 1-5; categories?; 
+// categories
+// Not sure how to create two separate windows; might make the quiz on a pop-out over the landing page 
+
+
+/*---------------- Event Listeners -------------------*/
+
+// clicks on categories and answers
+// extra 1: hover over on categories and answers
+// extra 2: click on pause button
+// toggle: light and dark mode
+// replay quiz
+
+
+/*------------------- Functions ---------------------*/
+
+// init to landing page
+// render to quiz page/popout
+// render to result page
+// check answer
+// change between questions
+// totaling results
+// extra: shuffle questions
+// extra: random quiz
+// extra: difficulties
