5cf5be6ed89563597bd21cd695ed85f506ac64c2
rhkearns <ryankearns217@gmail.com>
2021-10-13 22:29:55 -0400

    add questions for hp & lotr

---
+++ data/hp.js
@@ -0,0 +1,64 @@
+// Quesitons for Harry Potter Category
+// One object for each question
+
+const hpQuestions = [
+   {
+      "question": "What do Hermione's parents do for a living?",
+      "correctAns": "Dentists",
+      "multChoice": ["Green Grocers", "Dentists", "Bankers", "Chiropractors", "Drill Manufacturers"]
+   },
+
+   {
+      "question": "Which of these was not a monster that Harry faced in the maze in the Third Task of the Triwizard Tournament?",
+      "correctAns" : "",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What is Percy's Owl's name?",
+      "correctAns" : "Hermes",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What form was Ernie Macmillan's Patronus?",
+      "correctAns" : "Boar",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "Who of the following was not a member of the Order of the Phoenix?",
+      "correctAns" : "",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What is Dean Thomas' favorite Football (soccer) team?",
+      "correctAns" : "West Ham United",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "How many 'Outstanding' O.W.L.'s did Hermione receive?",
+      "correctAns" : "10",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What street does Severus Snape live on?",
+      "correctAns" : "Spinner's End",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What model broomstick does Ron receive in Order of the Phoenix?",
+      "correctAns" : "Cleansweep Eleven",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "How many staircases are there at Hogwarts?",
+      "correctAns" : "142",
+      "multChoice" : []
+   },
+]
---
+++ data/lotr.js
@@ -0,0 +1,68 @@
+// Quesitons for Harry Potter Category
+// One object for each question
+
+const lotrQuestions = [
+
+   {
+      "question" : "What was the name of Sauron's fortress in Mordor?",
+      "correctAns" : "Barad-D\xFBr",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "Who wrote the poem 'All that is gold does not glitter, Not all those who wander are lost'?",
+      "correctAns" : "Bilbo Baggins",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What is the name of the innkeep at The Prancing Pony?",
+      "correctAns" : "Barliman Butterbur",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What is the name of the great river that the Fellowship rows down after leaving Lothlorien?",
+      "correctAns" : "Anduin",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "Who cut the One Ring from Sauron's hand during the Battle of the Last Alliance?",
+      "correctAns" : "Isildur",
+      "multChoice" : ""
+   },
+
+   {
+      "question" : "What was the name of Shelob's mother?",
+      "correctAns" : "Ungoliant",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What is the name of Gandalf's sword?",
+      "correctAns" : "Glamdring",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "What is the date of Bilbo's and Frodo's birthday?",
+      "correctAns" : "September 22nd",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "At the end of Return of the King, who bore the Three Elvish Rings?",
+      "correctAns" : "Elrond, Galadriel, and Gandalf",
+      "multChoice" : []
+   },
+
+   {
+      "question" : "?",
+      "correctAns" : "",
+      "multChoice" : []
+   },
+
+
+
+]
