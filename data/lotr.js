5f0912939851a31e008aa10768816a987e4c9223
rhkearns <ryankearns217@gmail.com>
2021-10-17 16:54:20 -0400

    lotr questions done

--- data/lotr.js
+++ data/lotr.js
@@ -5,62 +5,62 @@ const lotrQuestions = [
 
    {
       "question" : "What was the name of Sauron's fortress in Mordor?",
-      "correctAns" : "Barad-D\xFBr",
-      "multChoice" : []
+      "correctAns" : "Barad-D&ucircr",
+      "multChoice" : ["Orthanc", "Barad-D&ucircr", "Cirith Ungol", "Dor L&oacutemon"]
    },
 
    {
       "question" : "Who wrote the poem 'All that is gold does not glitter, Not all those who wander are lost'?",
       "correctAns" : "Bilbo Baggins",
-      "multChoice" : []
+      "multChoice" : ["Thorin Oakenshield", "Arwen Und&oacutemiel", "Bilbo Baggins", "Merry Brandybuck", "L&uacutethien Tin&uacuteviel"]
    },
 
    {
       "question" : "What is the name of the innkeep at The Prancing Pony?",
       "correctAns" : "Barliman Butterbur",
-      "multChoice" : []
+      "multChoice" : ["Barliman Butterbur", "Tom Bombadil", "Farmer Maggot", "Fatty Bolger", "Bill Ferny"]
    },
 
    {
-      "question" : "What is the name of the great river that the Fellowship rows down after leaving Lothlorien?",
+      "question" : "What is the name of the Great River that the Fellowship rows down after leaving Lothlorien?",
       "correctAns" : "Anduin",
-      "multChoice" : []
+      "multChoice" : ["Celduin", "Isen", "Bruinen", "Anduin", "Entwash"]
    },
 
    {
       "question" : "Who cut the One Ring from Sauron's hand during the Battle of the Last Alliance?",
       "correctAns" : "Isildur",
-      "multChoice" : ""
+      "multChoice" : ["Elendil", "Arathorn", "Beren", "Isildur", "Elrond"]
    },
 
    {
-      "question" : "What was the name of Shelob's mother?",
+      "question" : "Who is Shelob the last child of?",
       "correctAns" : "Ungoliant",
-      "multChoice" : []
+      "multChoice" : ["Melkor", "Yavanna", "Melian", "Nienna", "Ungoliant"]
    },
 
    {
       "question" : "What is the name of Gandalf's sword?",
       "correctAns" : "Glamdring",
-      "multChoice" : []
+      "multChoice" : ["Anduril", "Orcrist", "Glamdring", "Gurthang", "Herugrim"]
    },
 
    {
       "question" : "What is the date of Bilbo's and Frodo's birthday?",
       "correctAns" : "September 22nd",
-      "multChoice" : []
+      "multChoice" : ["September 22nd", "October 24th", "August 1st", "April 9", "June 18th"]
    },
 
    {
       "question" : "At the end of Return of the King, who bore the Three Elvish Rings?",
       "correctAns" : "Elrond, Galadriel, and Gandalf",
-      "multChoice" : []
+      "multChoice" : ["Aragorn, Arwen, and Galadriel", "Bilbo, C&iacuterdan, and Celebrimbor", "Galadriel, Celeborn, and Glorfindel", "Elrond, Galadriel, and Gandalf", "Vilya, Nenya, Narya"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "How old is Frodo when he sets out from the Shire with the Ring?",
+      "correctAns" : "50",
+      "multChoice" : ["25", "33", "41", "50", "67"]
    },
 
 
