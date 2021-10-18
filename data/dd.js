e76ef3765ab0542c8db73fc966c4789259348eb6
rhkearns <ryankearns217@gmail.com>
2021-10-18 10:12:35 -0400

    dd questions done and functional

--- data/dd.js
+++ data/dd.js
@@ -4,65 +4,63 @@
 const ddQuestions = [
    
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "When can monsters use Legendary Actions?",
+      "correctAns" : "At the end of Player's turns",
+      "multChoice" : ["At any point on its turn", "At the beginning of its own turn", "At the end of Player's turns", "On player turns immediately after being attacked", "At the beginning of each round"]
    },
    
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "Unicorns are immune to which damage type?",
+      "correctAns" : "Poison",
+      "multChoice" : ["Poison", "Fire", "Necrotic", "Cold", "Acid"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
-   },
-
-   
-
-   {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "Which of the following is not a Metamagic option?",
+      "correctAns" : "Beguiling Spell",
+      "multChoice" : ["Distant Spell", "Quickened Spell", "Twinned Spell", "Heightened Spell", "Beguiling Spell"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "At what spell slot level does Dispel Magic work automatically?",
+      "correctAns" : "4",
+      "multChoice" : ["8", "5", "4", "2", "7"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "At what age do Tortles reach adulthood?",
+      "correctAns" : "15",
+      "multChoice" : ["27", "21", "15", "5", "39"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What effect does 4 levels of Exhaustion have on a character?",
+      "correctAns" : "Hit Point Maximum Halved",
+      "multChoice" : ["Hit Point Maximum Halved", "Disadvantage on Ability Checks", "Speed Reduced to 0", "Speed Halved", "Disadvantage on Attack Rolls and Saving Throws"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "How many wildshapes can a Druid use per day at level 18?",
+      "correctAns" : "2",
+      "multChoice" : ["4", "2", "9", "18", "12"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "At what level can a Cleric use Divine Intervention?",
+      "correctAns" : "10",
+      "multChoice" : ["5", "18", "15", "10", "20"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What bonus does 3/4 cover give?",
+      "correctAns" : "+5 bonus to AC and Dexterity Saving Throws",
+      "multChoice" : ["Cannot be targeted by attack or spell", "+2 bonus to AC and Dexterity Saving Throws", "+3 to Stealth Ability Checks", "-5 to perception checks to see the character", "+5 bonus to AC and Dexterity Saving Throws"]
+   },
+
+   {
+      "question" : "Which card from the Deck of Many Things has this effect: 'Every magic item you wear or carry disintegrates. Artifacts in your possession aren't destroyed but do vanish?",
+      "correctAns" : "Talons",
+      "multChoice" : ["Talons", "Flames", "Moon", "Vizier", "Comet"]
    },
 
 ]
