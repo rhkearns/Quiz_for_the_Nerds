88589ca3c927964f909a13297ba405f32c1b0ac6
rhkearns <ryankearns217@gmail.com>
2021-10-17 22:48:14 -0400

    mcu questions done and functional

--- data/mcu.js
+++ data/mcu.js
@@ -4,65 +4,65 @@
 const mcuQuestions = [
    
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "Which high school does Peter Parker attend?",
+      "correctAns" : "Midtown School of Science and Technology",
+      "multChoice" : ["Queens High School for the Sciences", "Midtown School of Science and Technology", "Staten Island Technical High School", "Brooklyn Technical High School", "Bronx High School of Science"]
    },
    
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "At the end of Iron Man, what mateiral is the Iron Man suit made from?",
+      "correctAns" : "Gold-Titanium Alloy",
+      "multChoice" : ["Cubic Zirconium", "Chrome-plated Iron", "Vibranium", "Gold-Titanium Alloy", "Nickel-Cobalt Alloy"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What New Mexico town does Thor arrive in?",
+      "correctAns" : "Puente Antiguo",
+      "multChoice" : ["San Ysidro", "Puente Antiguo", "Alamogordo", "Folsom", "Jemez Springs"]
    },
 
    
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What was the name of the boat on which Captain America first fights Batroc?",
+      "correctAns" : "Lemurian Star",
+      "multChoice" : ["USS Emerson", "S.S. Recovery", "Lemurian Star", "Conquistador", "HMS Albion"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What is the name of the Kree homeworld?",
+      "correctAns" : "Hala",
+      "multChoice" : ["Contraxia", "Alfheim", "Morag", "Hala", "Spartax"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What is the name of the prison that the Guardians of the Galaxy escape from?",
+      "correctAns" : "The Kyln",
+      "multChoice" : ["The Kyln", "The Milano", "The Raft", "The Fridge", "The Slingshot"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "In which order does Thanos assemble the Infinity Stones?",
+      "correctAns" : "Power-Space-Reality-Soul-Time-Mind",
+      "multChoice" : ["Soul-Time-Mind-Reality-Space-Power", "Time-Space-Power-Soul-Space-Mind", "Reality-Space-Time-Mind-Soul-Power", "Mind-Power-Reality-Soul-Space-Time", "Power-Space-Reality-Soul-Time-Mind"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "What is the name of the Dora Milaje who helped Bucky Barnes remove Hydra's programming?",
+      "correctAns" : "Ayo",
+      "multChoice" : ["Nomble", "Ayo", "Yama", "Okoye", "Xoliswa"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "In how many Marvel Cinematic Universe films has Nick Fury appeared?",
+      "correctAns" : "11",
+      "multChoice" : ["4", "8", "19", "11", "14"]
    },
 
    {
-      "question" : "?",
-      "correctAns" : "",
-      "multChoice" : []
+      "question" : "In Black Widow, Yelena mistakenly calls Alexei by which superhero's name?",
+      "correctAns" : "Crimson Dynamo",
+      "multChoice" : ["Crimson Dynamo", "Northstar", "Sasquach", "Darkstar", "Sputnik"]
    },
 
 ]
